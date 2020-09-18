c4d952e073a03a167e1df4c8e6c428cafbe84e54
Sher6J <liujx17614417627@outlook.com>
2020-09-18 12:10:21 +0800

    createThread

---
+++ src/main/java/cn/sher6j/createThread/StartAndRun.java
@@ -0,0 +1,27 @@
+package cn.sher6j.createThread;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 开始一个新线程需要用start()，而不能直接调用run()
+ * 直接调用 run 是在主线程中执行了 run，没有启动新的线程
+ * 使用 start 是启动新的线程，通过新的线程间接执行 run 中的代码
+ * @author sher6j
+ * @create 2020-09-18-12:00
+ */
+@Slf4j(topic = "c.StartAndRun")
+public class StartAndRun {
+    public static void main(String[] args) {
+        Thread t1 = new Thread("t1") {
+            @Override
+            public void run() {
+                log.debug("running...");
+            }
+        };
+
+        // run()并没有启动一个新的线程，而是由主线程执行的run方法，不能达到提升性能、异步处理的效果
+        t1.run(); // [main] c.StartAndRun - running...
+
+        t1.start(); // [t1] c.StartAndRun - running...
+    }
+}
---
+++ src/main/java/cn/sher6j/createThread/ThreadStatus.java
@@ -0,0 +1,23 @@
+package cn.sher6j.createThread;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author sher6j
+ * @create 2020-09-18-12:07
+ */
+@Slf4j(topic = "c.ThreadStatus")
+public class ThreadStatus {
+    public static void main(String[] args) {
+        Thread t1 = new Thread("t1") {
+            @Override
+            public void run() {
+                log.debug("running...");
+            }
+        };
+
+        System.out.println(t1.getState()); // NEW
+        t1.start();
+        System.out.println(t1.getState()); // RUNNABLE
+    }
+}
